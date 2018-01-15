f5e763a270e214ea3be260ef5584145ff9452969
Manjunatha Shety H <manjunatha.shetty_h@nokia.com>
2018-01-15 11:30:54 +0530

    Refactored and removed fmt for log

--- helloworld/client/client.go
+++ helloworld/client/client.go
@@ -9,23 +9,16 @@ import (
 
 func main() {
 	conn, err := grpc.Dial("localhost:17002", grpc.WithInsecure())
-
 	if err != nil {
 		log.Fatalf("Failed to connect to server")
 	}
-
 	client := helloworld.NewHelloWorldServiceClient(conn)
-
 	if err != nil {
 		log.Fatalf("Failed to connect to server")
 	}
-
 	request := helloworld.HelloRequest{Message: "Hi"}
-
 	resp, err := client.SayHello(context.Background(), &request)
-
 	log.Println("Got response ", resp.Message)
-
 	conn.Close()
 
 }
--- helloworld/server/server.go
+++ helloworld/server/server.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"golang.org/x/net/context"
@@ -10,18 +9,14 @@ import (
 )
 
 func main() {
-	fmt.Println("Staring server...")
+	log.Println("Staring server...")
 	srv := TestServiceImpl{}
 	lis, err := net.Listen("tcp", ":17002")
-
 	if err != nil {
 		log.Fatalf("Failed to bind to port %v", err)
 	}
-
 	s := grpc.NewServer()
-
 	helloworld.RegisterHelloWorldServiceServer(s, &srv)
-
 	log.Println("Starting the server ...")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to start server")
@@ -32,7 +27,7 @@ type TestServiceImpl struct {
 }
 
 func (*TestServiceImpl) SayHello(ctx context.Context, in *helloworld.HelloRequest) (*helloworld.HelloResponse, error) {
-	fmt.Println("Say Hello called")
+	log.Println("Say Hello called")
 	response := &helloworld.HelloResponse{Message: "Hello"}
 	return response, nil
 }
