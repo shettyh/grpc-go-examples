d272d970879a693f4e096cf227ae5ddde8ed4d26
Manjunatha Shety H <manjunatha.shetty_h@nokia.com>
2018-01-15 10:49:06 +0530

    Added initial client interceptor implementation

--- interceptor/client/client.go
+++ interceptor/client/client.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"log"
+	"github.com/shettyh/grpc-go-examples/interceptor"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"github.com/shettyh/grpc-go-examples/interceptor"
+	"log"
+	"time"
 )
 
 func main() {
-	conn, err := grpc.Dial("localhost:17002", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:17002", grpc.WithInsecure(), WithClientInterceptor())
 
 	if err != nil {
 		log.Fatalf("Failed to connect to server")
@@ -28,4 +29,16 @@ func main() {
 
 	conn.Close()
 
+}
+
+func WithClientInterceptor() grpc.DialOption {
+	return grpc.WithUnaryInterceptor(clientInterceptor)
+}
+
+func clientInterceptor(ctx context.Context, method string, req, reply interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	log.Println("Intercepting the remote call ",method)
+	start := time.Now()
+	err := invoker(ctx, method, req, reply, cc, opts...) // <==
+	log.Printf("invoke remote method=%s duration=%s error=%v", method, time.Since(start), err)
+	return err
 }
